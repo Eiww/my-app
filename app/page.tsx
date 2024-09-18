9fe79bd989c209ae5a66d14aa1b082493421d2f8
Eiww <niktonikakoj0@gmail.com>
2024-09-19 08:44:12 +0900

    fix header

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <header className="row-start-1 flex justify-center gap-6">
-        <nav className="flex">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 pb-10 gap-4 sm:p-8 sm:pb-20 sm:gap-8 font-[family-name:var(--font-geist-sans)]">
+      <header className="row-start-1 flex flex-col items-center gap-4 sm:flex-row sm:items-start sm:gap-6">
+        <nav className="flex flex-wrap gap-4 sm:gap-6">
           <a href="#" className="text-white font-bold">
             О нас
           </a>
@@ -25,11 +25,11 @@ export default function Home() {
           </a>
         </nav>
       </header>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <p>WAVE</p>
-        <p>школа для серфинга</p>
+      <main className="flex flex-col gap-4 row-start-2 items-center sm:items-start">
+        <p className="text-5xl sm:text-6xl font-bold">WAVE</p>
+        <p className="text-lg sm:text-xl font-medium">школа для серфинга</p>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+      <footer className="row-start-3 flex gap-4 flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
