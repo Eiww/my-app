d73d60ca67fd0825f0064a1b4f1d201b799497b9
Eiww <niktonikakoj0@gmail.com>
2024-09-19 09:10:27 +0900

    ura okno tailwind complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,13 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+  src: "./fonts/RobotoCondensed-VariableFont_wght.ttf",
+  variable: "--font-robo-cond",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+  src: "./fonts/RobotoCondensed-Italic-VariableFont_wght.ttf",
+  variable: "--font-robo-ital",
   weight: "100 900",
 });
 
--- app/page.tsx
+++ app/page.tsx
@@ -5,22 +5,22 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 pb-10 gap-4 sm:p-8 sm:pb-20 sm:gap-8 font-[family-name:var(--font-geist-sans)]">
       <header className="row-start-1 flex flex-col items-center gap-4 sm:flex-row sm:items-start sm:gap-6">
         <nav className="flex flex-wrap gap-4 sm:gap-6">
-          <a href="#" className="text-white font-bold">
-            О нас
+          <a href="#" className="text-white hover:text-blue-300">
+            О нас 0
           </a>
-          <a href="#" className="text-white font-bold">
+          <a href="#" className="text-white">
             Услуги
           </a>
-          <a href="#" className="text-white font-bold">
+          <a href="#" className="text-white">
             Стоимость
           </a>
-          <a href="#" className="text-white font-bold">
+          <a href="#" className="text-white">
             Блог
           </a>
-          <a href="#" className="text-white font-bold">
+          <a href="#" className="text-white">
             Команда
           </a>
-          <a href="#" className="text-white font-bold">
+          <a href="#" className="text-white">
             Контакты
           </a>
         </nav>
