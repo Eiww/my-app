f2450e7f2ef144484eaba39c8293df7dd2682684
Eiww <niktonikakoj0@gmail.com>
2024-10-08 09:42:31 +0900

    update 2 & 3 slide

--- app/page.tsx
+++ app/page.tsx
@@ -60,15 +60,17 @@ export default function Home() {
           <footer className="row-start-3 flex gap-4 flex-wrap items-center justify-center"></footer>
         </main>
       </div>
-      <div className="grid grid-rows-[40px_1fr_40px] items-center justify-items-center min-h-screen p-10 pb-10 gap-4 ">
-        <p className="absolute text-darkblue user-select-none select-none text-5xl right-32 top-[1038px] sm:top-[1000px] sm:right-60">
-          /
-        </p>
-        <p className=" text-black user-select-none select-none sm:text-justify pt-44 text-3xl text-left">
-          О ШКОЛЕ, О СЕРФИНГЕ, О КАНАРАХ
-        </p>
-
-        {/* <div className="grid grid-cols-6 gap-12 md:grid-cols-2 p-80">
+      <div className="grid items-center content-between justify-items-center min-h-screen p-10 pb-10 gap-4 ">
+        <div className="flex items-end">
+          <p className=" text-black user-select-none select-none sm:text-justify text-3xl text-left">
+            О ШКОЛЕ, О СЕРФИНГЕ, О КАНАРАХ
+          </p>
+          <p className="text-darkblue user-select-none select-none text-5xl">
+            /
+          </p>
+        </div>
+      </div>
+      {/* <div className="grid grid-cols-6 gap-12 md:grid-cols-2 p-80">
           <p className=" text-black">
             Канарские острова не случайно называют европейскими Гавайями – это
             идеальное место для поклонников серфинга: стабильный теплый ветер
@@ -120,7 +122,6 @@ export default function Home() {
             великолепных ощущений, но и это уровень поднимется на новую ступень.
           </p>
         </div> */}
-      </div>
       {/* <Swiper
         spaceBetween={50}
         slidesPerView={3}
@@ -133,13 +134,16 @@ export default function Home() {
         <SwiperSlide>Slide 4</SwiperSlide>
         ...
       </Swiper> */}
-      <div className="grid grid-rows-[40px_1fr_40px] items-center justify-items-center min-h-screen p-10 pb-10 gap-28 bg-bggray">
-        <p className=" text-black user-select-none select-none sm:text-justify md:pt-44 text-3xl text-left">
-          В ШКОЛЕ ВЫ ПОЛУЧИТЕ
-        </p>
-
-        <ul className="list-none flex flex-col gap-24 md:flex md:flex-row md:items-center">
-          <div></div>
+      <div className="grid content-between items-center justify-items-center min-h-screen p-10 pb-10 gap-10 md:gap-28 bg-bggray">
+        <div className="flex items-end">
+          <p className=" text-black user-select-none select-none sm:text-justify text-3xl text-left">
+            В ШКОЛЕ ВЫ ПОЛУЧИТЕ
+          </p>
+          <p className="text-darkblue user-select-none select-none text-5xl">
+            /
+          </p>
+        </div>
+        {/* <ul className="list-none flex flex-col gap-24 md:flex md:flex-row md:items-center ">
           <li className="flex items-center flex-col md:flex-row-reverse gap-5">
             ПРОЖИВАНИЕ
             <img src="/img/icon_3s1.svg" alt="" />
@@ -168,29 +172,78 @@ export default function Home() {
               начинается с определения вашего уровня катания
             </p>
           </li>
-        </ul>
-        <div className="gap-8">
-          <div>
+        </ul> */}
+        <div className="flex md:grid md:grid-cols-3">
+          <div className="flex flex-col md:flex-row justify-center items-center">
+            <p>ПРОЖИВАНИЕ</p>
+            <img src="/img/icon_3s1.svg" alt="" />
+          </div>
+
+          <div className="flex flex-row justify-center items-center">
+            <p>ПЕРЕЛЕТ И СТРАХОВКУ</p>
+            <img src="/img/icon_3s2.svg" alt="" />
+          </div>
+
+          <div className="flex flex-row justify-center items-center">
+            <p>ОБУЧЕНИЕ И ЭКИПИРОВКУ</p>
+            <img src="/img/icon_3s3.svg" alt="" />
+          </div>
+
+          <p className="text-xs">
+            Наша школа серфинга находится в 100 м от океанского побережья. К
+            услугам гостей открытый бассейн и бесплатный Wi-Fi. Из верхней
+            террасы открывается вид на океан. Расстояние до центра города 15
+            мин.
+          </p>
+          <p className="text-xs">
+            Все участники программы застрахованы страховой компанией Allianz.
+            Перелет из Тенерифе до острова Лансароте и обратно входит в
+            стоимость программы
+          </p>
+          <p className="text-xs">
+            Все оборудование и экипировка известных производителей и
+            предоставляется на весь курс программы. Тренировочный процесс
+            начинается с определения вашего уровня катания
+          </p>
+        </div>
+        <div className="flex flex-col md:flex md:flex-row md:justify-around md:items-center">
+          <div className="flex flex-col gap-4">
             <div>
-              <p>приехали</p>
               <img src="/img/wave85.svg" alt="" />
               <p>85 раз</p>
             </div>
             <div>
-              <p>приехали</p>
+              <p>приехали второй раз</p>
+            </div>
+
+            <div>
               <img src="/img/wave95.svg" alt="" />
               <p>95 раз</p>
             </div>
             <div>
-              <p>приехали</p>
+              <p>научились кататься</p>
+            </div>
+            <div>
               <img src="/img/wave99.svg" alt="" />
               <p>99 раз</p>
             </div>
+            <div>
+              <p>довольных клиентов</p>
+            </div>
           </div>
-          <div>
-            {" "}
-            <p>приехали</p>
-            <p>приехали</p>
+          <div className="md: flex flex-col md:w-1/3  md:p-2 gap-4">
+            <p>
+              Огромным преимуществом нашей школы является собственная методика
+              обучения. Мы разработали собственный стиль обучения серфингу,
+              который позволяет новичкам максимально быстро встать на доску.
+            </p>
+            <p>
+              Именно наша методика и наши преподаватели позволили нам пройти
+              аттестацию Федерации серфинга Канарских островов - FEDERACIÓN
+              CANARIA DE SURF. Это означает, что наша школа соответствует всем
+              мировым стандартам. Переаттестация производится ежегодно и это
+              позволяет нам всегда стремиться к улучшению обучения.
+            </p>
           </div>
         </div>
       </div>
